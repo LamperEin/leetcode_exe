619ceaa49febb228faadf4aa011ba58f60a319a2
LamperEin <1161299387@qq.com>
2020-03-09 13:55:22 +0800

    "solved hammingWeight problem."

--- algo_solver/miscellaneous_solver.cpp
+++ algo_solver/miscellaneous_solver.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 int hammingWeight(uint32_t n) {
-    
+    int count = 0;
+    while(n > 0) {
+        count++;
+        n = (n-1) & n;
+    }
+    return count;
 }
 
 
 int main() {
-
+    uint32_t a = 67;
+    int res = hammingWeight(a);
+    cout << res <<endl;
     return 0;
 }
